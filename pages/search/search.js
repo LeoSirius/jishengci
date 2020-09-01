44d14e654077022ac25f61e602961efeb7c2a3b0
LeoSirius <is.li.xiaoyu@qq.com>
2020-09-01 22:34:31 +0800

    update

--- pages/search/search.js
+++ pages/search/search.js
@@ -5,8 +5,26 @@ Page({
    * Page initial data
    */
   data: {
+    keyboardHeight: 100,
+    apiUrl: 'http://39.106.229.224/api/words/',
     word:{"name":"jack",
     "meanings":[{"pos":"n.","meaning":"千斤顶；插座；作动简；起重器"},{"pos":"v.","meaning":"张拉；用起重器抬起；偷(车)"},{"pos":"adj.","meaning":"厌烦"},{"pos":"网络","meaning":"杰克；家有杰克；升降机"}]}
+    // word: {},
+  },
+
+
+  handleSearchInput: function(e) {
+    let that = this;
+    let word = e.detail.value;
+    wx.request({
+      url: this.data.apiUrl + word + '/',
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success (res) {
+        that.setData({word: res.data});
+      }
+    })
   },
 
   /**
